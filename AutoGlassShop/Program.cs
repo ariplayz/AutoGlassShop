afa0b54ee0716afc43607011141ca7cecfefd1ed
Ari Cummings <ari@arigreenecummings.com>
2025-05-12 14:30:56 -0700

    Optimized

--- AutoGlassShop/Program.cs
+++ AutoGlassShop/Program.cs
@@ -42,3 +42,18 @@ class Program
         }
     }
 }
+
+public class GetCompetitorPrice
+{
+    public static double getGlassPrice(double competitorPrice)
+    {
+        double price;
+        double tax;
+        
+        tax = competitorPrice * 0.086;
+
+        price = competitorPrice - 55 - tax;
+
+        return price;
+    }
+}
